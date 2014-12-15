6c157f053fe41e1c363d5187afd04523b140b7a0
Divya Kondeti <Divya@Divyas-MacBook-Pro-3.local>
2014-12-15 15:08:55 +0530

    Initial Commit

---
+++ C GeniusPort/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  C GeniusPort
+//
+//  Created by Divya Kondeti on 15/12/2014.
+//  Copyright (c) 2014 Divya Kondeti. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
